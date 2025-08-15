a5a819f840205b27c0ee1c19b717a1462bc33670
Akshat1473 <akshat8958@gmail.com>
2025-08-15 23:51:02 +0530

    final commit

--- API/Controllers/payment.js
+++ API/Controllers/payment.js
@@ -6,8 +6,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const razorpay = new Razorpay({
-  key_id:"rzp_test_R5gGrbIihDr41X" ,
-  key_secret: "zHcANu0a8FKd7OQB3i3QqovS",
+  key_id:process.env.RAZORPAY_KEY_ID ,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 }); 
 
 // checkout
